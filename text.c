0daa92647d866c5f76593d94d77d4e20647caccd
sekirosaber <161699913+sekirosaber@users.noreply.github.com>
2024-03-02 20:34:53 +0800

    Add files via upload

---
+++ text.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	//printf("here\n");
+	//printf("胡桃\n");
+	//printf("%d\n", 100);//打印一各整數
+
+	printf("%d\n", sizeof(char));
+	printf("%d\n", sizeof(short));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(long long));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(double));
+	return 0;
+}
